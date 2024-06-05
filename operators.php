bcc85739108a9d0147958c13dd0e11d7c2ff0d16
Githaka <githakamaru@gmail.com>
2024-06-05 15:53:56 +0300

    add a PHP Opertors exrcise file

---
+++ operators.php
@@ -0,0 +1,15 @@
+<!--PHP Operator is a symbol i.e used to perform operations on operands. -->
+<!--In simple words, operators are used to perform operations on variables or values.-->
+
+<!--Arithmetic operators in PHP-->
+<!---->
+<!--Operation      Operator     Example       Result-->
+<!--_________________________________________________-->
+<!--Addition        +           $x + 10        30-->
+<!--Subtraction     -           $x - 10        10-->
+<!--Multiplication  *           $x * 10        200-->
+<!--Division        /           $x / 10         2-->
+<!--Modulo          %           $x % 3          2 -->
+<!--Increment   (add 1) ++      $x++ 21 -->
+<!--Decrement (subtract 1) --   $x-- 19 -->
+<!--Exponentiation **           $x**3          8000-->
