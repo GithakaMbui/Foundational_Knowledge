04ad9391bc4b81d5d74e7183cc1572e2001a7cdf
Githaka <githakamaru@gmail.com>
2024-06-19 23:47:32 +0300

    add a functions file

---
+++ functions.php
@@ -0,0 +1,51 @@
+<!--Example 1-->
+-----------
+<?php
+function displayGreetings(){ // here we declare a function called displayGreetings() that has no parameters (
+    echo 'Hello';
+}
+displayGreetings();  //To call the function, we use its name followed by a pair of parentheses:
+echo "</br>";  echo "</br>";
+?>
+
+//Example 2
+-------------
+<?php
+function displayGreetings2($name, $message){
+    echo "Hello $name, $message";
+}
+displayGreetings2('Jamie', 'good morning');
+echo "</br>";  echo "</br>";
+?>
+
+
+//Example 3
+<!----------------->
+//We can provide default values for parameter(s) when we declare our functions.
+//When we declare functions with default values for parameters,
+//note that parameters with default values must come after parameters without default values
+
+<?php
+function displayGreetings3($name, $message = 'have a good day')
+{ echo "Hello $name, $message";
+}
+displayGreetings3('Jamie');
+echo '<BR>';
+displayGreetings3('Jamie', 'how are you?');
+echo "</br>";  echo "</br>";
+?>
+
+<!--Example 4-->
+------------
+//functions can return a result after they complete their tasks.
+<?php
+function addNumbers($num1, $num2, $num3)
+{
+    return $num1 + $num2 + $num3;
+    echo 'Hello';
+}
+echo addNumbers(9, 6, 1);
+//We can assign the result returned by the
+//function to a variable or use the echo statement to display the result directly
+echo "</br>";  echo "</br>";
+?>
